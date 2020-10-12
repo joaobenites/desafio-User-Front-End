2a78628c50f82e20428b5781f31b9f73a419098e
joaobenites <joaofelipe22.jf@gmail.com>
2020-10-11 22:24:17 -0300

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { MatTableDataSource } from '@angular/material/table';
+import { CreateUserComponent } from '../create-user/create-user.component';
 import { User } from '../models/user';
 import { UserService } from '../service/user.service';
 @Component({
@@ -13,10 +15,9 @@ export class HomeComponent implements OnInit{
   title: string;
   users: User[];
   dataSource: MatTableDataSource<User>;
-  constructor(private userService:UserService) {
-    this.userService.findAll().subscribe(user => {this.users = user;  this.dataSource.data = user;   
-      
-    });
+  modal: MatDialog
+  constructor(private userService:UserService, public dialog: MatDialog) {
+      this.getUserList();
 
     this.title = 'Desafio Listagem';
   }
@@ -27,13 +28,29 @@ export class HomeComponent implements OnInit{
       return value;
     };
   }
+  getUserList(){
+    this.userService.findAll().subscribe(user => {this.users = user;  this.dataSource.data = user;   
+    });
+  }
 
-  displayedColumns: string[] = ['id', 'username', 'password', 'is_enabled', 'register_date', 'name', 'surname', 'email', 'phone'];
+  displayedColumns: string[] = ['id', 'username', 'password', 'is_enabled', 'register_date', 'name', 'surname', 'email', 'phone', 'edit','delete'];
 
   applyFilter(filterValue) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  editUser(user){
+    let dialogRef =  this.dialog.open(CreateUserComponent,{data:user});
+    dialogRef.afterClosed().subscribe(result => this.getUserList());
+  }
+  createUser(){
+    let dialogRef = this.dialog.open(CreateUserComponent,{});
+    dialogRef.afterClosed().subscribe(result => this.getUserList());
+  }
+  deleteUser(user){
+
+    this.userService.delete(user).subscribe(retorno => this.getUserList());
+  }
 
 
 }
